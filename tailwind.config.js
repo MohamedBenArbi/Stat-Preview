73514b70d269fda6d77936501e05948acb38c3bd
MohamedBenArbi <Mohamedbenarbi01@gmail.com>
2022-06-30 21:13:26 +0100

    test

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+
+      colors: {
+        ppl: {
+          100: '#371B58',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
